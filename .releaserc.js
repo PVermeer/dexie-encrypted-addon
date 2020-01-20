e8137263efbabc0d51bc03cc87efd609895b27ec
Pim Vermeer <pim@zzapps.nl>
2020-01-20 19:30:41 +0100

    fix(release): changelog + version should now update

--- .releaserc.js
+++ .releaserc.js
@@ -4,21 +4,15 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-    ],
-    [
-      "@semantic-release/npm",
-    ],
-    [
-      '@semantic-release/github',
-      {
-        assets: [
-          'CHANGELOG.md',
-          'package.json',
-          'package-lock.json'
-        ]
-      }
-    ]
+    '@semantic-release/changelog',
+    "@semantic-release/npm",
+    '@semantic-release/github',
+    '@semantic-release/git', {
+      assets: [
+        'CHANGELOG.md',
+        'package.json',
+        'package-lock.json'
+      ]
+    }
   ]
 };
